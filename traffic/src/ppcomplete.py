3d0b2741470f3686893b3ab6bf6491bcaf64890c
johnsgill3 <king2bgill@gmail.com>
2015-11-13 15:41:54 -0800

    Save Changes

--- traffic/src/ppcomplete.py
+++ traffic/src/ppcomplete.py
@@ -1,6 +1,6 @@
 import pickle
 import pprint
-BASE_DIR = "/video/dse_traffic"
+BASE_DIR = "/video/dse_capstone/traffic"
 PICKLE_FILENAME = BASE_DIR + "/completed_files.pkl"
 f = open(PICKLE_FILENAME, 'rb')
 completedFiles = pickle.load(f)
