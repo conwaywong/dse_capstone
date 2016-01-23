6b2c8a7ade8cc849f8ba338b221257c61cfd5141
Kevin Dyer <kkdyer8506@gmail.com>
2016-01-23 10:57:43 -0800

    update code to write streams; modify pom to create jar-with-dependencies as part of build

--- ml/scala/traffic/src/main/scala/org/ucsd/dse/capstone/traffic/PCAMain.scala
+++ ml/scala/traffic/src/main/scala/org/ucsd/dse/capstone/traffic/PCAMain.scala
@@ -1,24 +1,27 @@
 package org.ucsd.dse.capstone.traffic
 
 import java.io.BufferedWriter
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileWriter
+import java.io.InputStream
 import java.io.OutputStreamWriter
-import java.io.Writer
 
 import scala.collection.mutable.ListBuffer
 
-import org.apache.commons.io.IOUtils
 import org.apache.spark.SparkContext
 import org.apache.spark.annotation.Since
 import org.apache.spark.mllib.linalg.MatrixUDT
 import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.VectorUDT
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.stat.MultivariateStatisticalSummary
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.SQLUserDefinedType
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
 
 /**
  * @author dyerke
@@ -27,12 +30,25 @@ object PCAMain {
 
   def main(args: Array[String]) {
     val template: SparkTemplate = new DefaultSparkTemplate()
-    template.execute { sc => do_execute(sc) }
+    //
+    val output_aws_id = "AKIAJFOAU7QJI4CFLAKA"
+    val output_aws_secret_key = "6bBcmynL6SB0OeVX4+TUGecpQURjMu5cNqmqTaOi".replace("/", "%2F")
+    val cred: AWSCredentials = new BasicAWSCredentials(output_aws_id, output_aws_secret_key)
+    val client: AmazonS3 = new AmazonS3Client(cred)
+    //
+    template.execute { sc => do_execute(sc, client) }
   }
 
-  def do_execute(sc: SparkContext) = {
+  def do_execute(sc: SparkContext, client: AmazonS3) = {
 
-    val files: List[String] = List("/home/dyerke/Documents/DSE/capstone_project/traffic/data/01_2010", "/home/dyerke/Documents/DSE/capstone_project/traffic/data/01_2010_first_seven_days")
+    val aws_id = "AKIAJFOAU7QJI4CFLAKA"
+    val aws_secret_key = "6bBcmynL6SB0OeVX4+TUGecpQURjMu5cNqmqTaOi".replace("/", "%2F")
+    val bucket_name = "dse-jgilliii"
+    val obj_key = "dse_traffic/station_5min/2010/d12/d12_text_station_5min_2010_01_01.txt.gz"
+    val url = "s3n://%s:%s@%s/%s".format(aws_id, aws_secret_key, bucket_name, obj_key)
+    //
+    //    val files: List[String] = List("/home/dyerke/Documents/DSE/capstone_project/traffic/data/01_2010", "/home/dyerke/Documents/DSE/capstone_project/traffic/data/01_2010_first_seven_days")
+    val files: List[String] = List(url)
     val m_string_rdd: RDD[String] = MLibUtils.new_rdd(sc, files, 4)
 
     //
@@ -46,11 +62,11 @@ object PCAMain {
     m_fields_pca.foreach { tuple: Tuple2[String, Int] =>
       val file_dir_prefix = tuple._1
       val pivot_field = tuple._2
-      do_run(sc, m_string_rdd, fid, file_dir_prefix, pivot_field)
+      do_run(client, sc, m_string_rdd, fid, file_dir_prefix, pivot_field)
     }
   }
 
-  private def do_run(sc: SparkContext, m_string_rdd: RDD[String], fid: String, file_dir_prefix: String, pivot_field: Int) = {
+  private def do_run(client: AmazonS3, sc: SparkContext, m_string_rdd: RDD[String], fid: String, file_dir_prefix: String, pivot_field: Int) = {
     val handler: PivotHandler = new StandardPivotHandler(sc, pivot_field)
     val m_vector_rdd: RDD[Vector] = MLibUtils.pivot(m_string_rdd, handler)
     //
@@ -102,12 +118,7 @@ object PCAMain {
       val filename: String = tuple._1
       val stream: ByteArrayOutputStream = tuple._2
       //
-      val writer: Writer = new FileWriter(new File(filename))
-      try {
-        IOUtils.write(stream.toByteArray(), writer)
-      } finally {
-        IOUtils.closeQuietly(writer)
-      }
+      process_stream(client, filename, stream)
     }
     //
     // print statements to verify
@@ -122,5 +133,20 @@ object PCAMain {
       m_list_buffer += cum_sum
     }
     println("perc variance explained= " + m_list_buffer)
+  }
+
+  private def process_stream(client: AmazonS3, filename: String, stream: ByteArrayOutputStream): Unit = {
+    val output_bucket_name = "dse-csw009"
+    val simple_filename= filename.split("/").last
+    val output_bucket_key = "upload/" + simple_filename
+    //
+    val bytes: Array[Byte] = stream.toByteArray();
+    //
+    val in_stream: InputStream = new ByteArrayInputStream(bytes)
+    val in_stream_meta: ObjectMetadata = new ObjectMetadata()
+    in_stream_meta.setContentLength(bytes.length)
+    //
+    println("Invoking client.putObject with parameters: %s,%s".format(output_bucket_name, output_bucket_key))
+    client.putObject(output_bucket_name, output_bucket_key, in_stream, in_stream_meta)
   }
 }
