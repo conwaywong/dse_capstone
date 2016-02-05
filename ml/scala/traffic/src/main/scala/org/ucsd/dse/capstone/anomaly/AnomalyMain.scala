303f7d8345f78bd4fc874509ed3e62a73e52b689
Kevin Dyer <kkdyer8506@gmail.com>
2016-02-05 14:13:17 -0800

    fix compile error

--- ml/scala/traffic/src/main/scala/org/ucsd/dse/capstone/anomaly/AnomalyMain.scala
+++ ml/scala/traffic/src/main/scala/org/ucsd/dse/capstone/anomaly/AnomalyMain.scala
@@ -1,10 +1,16 @@
 package org.ucsd.dse.capstone.anomaly
 
 import org.apache.spark.SparkContext
+import org.apache.spark.annotation.Since
 import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
+
 import org.ucsd.dse.capstone.traffic.DefaultSparkTemplate
+import org.ucsd.dse.capstone.traffic.Fields
+import org.ucsd.dse.capstone.traffic.PivotHandler
 import org.ucsd.dse.capstone.traffic.SparkTemplate
+import org.ucsd.dse.capstone.traffic.StandardPivotHandler
 
 object AnomalyMain {
   private def parseVector(line: String): Vector =
@@ -18,21 +24,21 @@ object AnomalyMain {
   }
 
   def do_execute(sc: SparkContext) {
-    //        val files:List[String] = List("station_5min/d11_text_station_5min_2010_01_*.txt.gz")
-    //        val m_string_rdd:RDD[String] = MLibUtils.new_rdd(sc, files, 4)
-    //     
-    //        val handler:PivotHandler = new StandardPivotHandler(sc, Fields.Speed)
-    //        val m_vector_rdd:RDD[Vector] = MLibUtils.pivot(m_string_rdd, handler)
-    //        
-    //        //val files:List[String] = List("/Users/johngill/Documents/DSE/jgilliii/dse_capstone/ml/notebooks/t_data.csv")
-    //        //val m_vector_rdd:RDD[Vector] = MLibUtils.new_rdd(sc, files, 4).map(parseVector)
-    //        
-    //        val mahala:AnomalyDetector = new MahalanobisOutlier(sc)
-    //        mahala.fit(m_vector_rdd)
-    //        mahala.DetectOutlier(m_vector_rdd.zipWithIndex(), 3.0).collect().foreach(println)
-    //        
-    //        val kmeans:AnomalyDetector = new KMeansOutlier(sc, 10, 20)
-    //        kmeans.fit(m_vector_rdd)
-    //        kmeans.DetectOutlier(m_vector_rdd.zipWithIndex(), 4.5).collect().foreach(println)
+    val files: List[String] = List("station_5min/d11_text_station_5min_2010_01_*.txt.gz")
+    val m_string_rdd: RDD[String] = sc.textFile(files.mkString(","))
+
+    val handler: PivotHandler = new StandardPivotHandler(sc, Fields.Speed)
+    val m_vector_rdd: RDD[Vector] = handler.pivot(m_string_rdd)
+
+    //val files:List[String] = List("/Users/johngill/Documents/DSE/jgilliii/dse_capstone/ml/notebooks/t_data.csv")
+    //val m_vector_rdd:RDD[Vector] = MLibUtils.new_rdd(sc, files, 4).map(parseVector)
+
+    val mahala: AnomalyDetector = new MahalanobisOutlier(sc)
+    mahala.fit(m_vector_rdd)
+    mahala.DetectOutlier(m_vector_rdd.zipWithIndex(), 3.0).collect().foreach(println)
+
+    val kmeans: AnomalyDetector = new KMeansOutlier(sc, 10, 20)
+    kmeans.fit(m_vector_rdd)
+    kmeans.DetectOutlier(m_vector_rdd.zipWithIndex(), 4.5).collect().foreach(println)
   }
 }
